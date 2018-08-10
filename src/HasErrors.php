f3f971ff0443600f0665ae0ecc7ad266bc2dff0e
Steve Harmeyer <harmeyersteve@gmail.com>
2018-08-10 11:29:16 -0700

    initial commit

---
+++ src/HasErrors.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Submtd\ClassErrors;
+
+trait HasErrors
+{
+    protected $errors = [];
+
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    public function getLastError()
+    {
+        return end($this->errors);
+    }
+
+    public function getLastErrorMessage()
+    {
+        $error = end($this->errors);
+        return isset($error['message']) ? $error['message'] : null;
+    }
+
+    public function getLastErrorCode()
+    {
+        $error = end($this->errors);
+        return isset($error['message']) ? $error['message'] : null;
+    }
+
+    public function hasErrors()
+    {
+        return (bool) $this->errors;
+    }
+
+    private function addError($message, $code = 0)
+    {
+        $this->errors[] = [
+            'message' => $message,
+            'code' => $code,
+        ];
+    }
+}
